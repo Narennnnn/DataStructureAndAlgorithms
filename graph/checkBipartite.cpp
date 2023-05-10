637812ecbf0dd0a2076e9683260454e831528f65
Narendra Maurya <mauryanarendra2003@gmal.com>
2023-05-11 01:07:36 +0530

    save changes

--- graph/checkBipartite.cpp
+++ graph/checkBipartite.cpp
@@ -2,30 +2,28 @@
 using namespace std;
 bool bfs(int adjM[5][5], int source)
 {
-    bool visited[5] = {false};
-    stack<int> st;
-    st.push(s);
-    visited[s] = true;
-
-    while (!st.empty())
+    char color[5] = {'n', 'n', 'n', 'n', 'n'}; // initially all colour nil
+    queue<int> q;
+    set<int> U, V;
+    q.push(source);
+    color[source] = 'r'; // r for red
+    while (!q.empty())
     {
-        int curr = st.top();
-        st.pop();
-        if (curr == d)
-            return true;
-
-        // get the adjacent neighbors of the current node
+        int curr = q.front();
+        q.pop();
         for (int i = 0; i < 5; i++)
         {
-            if (adjM[curr][i] != 0 && !visited[i])
+            if (adjM[curr][i] && color[i] == 'n')
             {
-                // add the adjacent node to the stack and mark it as visited
-                st.push(i);
-                visited[i] = true;
+                color[i] = (color[curr] == 'r') ? 'b' : 'r';
+                q.push(i);
             }
+            if (color[curr] == color[i])
+                return false;
         }
     }
-    return false;
+
+    return true;
 }
 
 int main()
@@ -36,7 +34,7 @@ int main()
          {1, 1, 0, 1, 0},
          {0, 1, 1, 0, 1},
          {0, 1, 0, 1, 0}};
-    if (bfs(adjM, s, d))
+    if (bfs(adjM, 0))
     {
         cout << "Yes Bipartite\n";
     }
